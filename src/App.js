aa8b569399ae071f1b5568619dba9d9feffad7b4
aaron <aaron@pinkaloo.comn>
2020-10-18 17:27:31 -0700

    feat: switch to @material-ui for UI components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
 
+// Contracts and Web3
 import { simpleStorage } from "./abi/simpleStorage.js";
 import Web3 from "web3";
 
+// UI Components
+import { Button, TextField } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles'
+
+// Styles
 import "./App.css";
 
 const web3 = new Web3(Web3.givenProvider);
@@ -11,7 +17,16 @@ const web3 = new Web3(Web3.givenProvider);
 const contractAddress = "0xa0bc61bfb147751628459f7ea4b8e4626cc71ff4";
 const simpleContract = new web3.eth.Contract(simpleStorage, contractAddress);
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+ }));
+
 function App() {
+  const classes = useStyles();
   const [number, setUint] = useState(0);
   const [getNumber, setGet] = useState("0");
 
@@ -33,30 +48,39 @@ function App() {
   };
 
   return (
-    <div className="main">
-      <div className="card">
-        <form className="form" onSubmit={numberSet}>
-          <label>
-            Set your uint256:
-            <input
-              className="input"
-              type="text"
-              name="name"
-              onChange={(t) => setUint(t.target.value)}
-            />
-          </label>
-          <button className="button" type="submit" value="Confirm">
-            Confirm
-          </button>
-        </form>
-        <br />
-        <button className="button" onClick={numberGet} type="button">
-          Get your uint256
-        </button>
-        {getNumber}
+    <div className={classes.root}>
+      <div className="main">
+        <div className="card">
+          <TextField
+            id="outlined-basic"
+            label="Set your uint256:"
+            onChange={(t) => setUint(t.target.value)}
+            variant="outlined"
+          />
+          <form className="form" onSubmit={numberSet}>
+            <Button
+              variant="contained"
+              color="primary"
+              type="submit"
+              value="Confirm"
+            >
+              Confirm
+            </Button>
+            <br />
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={numberGet}
+              type="button"
+            >
+              Get your uint256
+            </Button>
+            {getNumber}
+          </form>
+        </div>
       </div>
     </div>
   );
-}
-
-export default App;
+ }
+ 
+ export default App;
